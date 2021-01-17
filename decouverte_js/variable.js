5353f213803cade344fefcab9de0b28d23fd6bf7
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-01-17 12:00:45 +0100

    TP_spoiler=code_html

---
+++ decouverte_js/variable.js
@@ -0,0 +1,14 @@
+//les variables
+var chiffre = 3;
+var demo = "chaine de caractère"
+var bool = true
+//tableau
+var eleve = ['jean', 'momo', 'kevin']
+//objet
+var eleve = {nom: 'momo', age: 34, moyenne: 14}
+eleve.nom // momo
+eleve.age // 34
+//pour ajouter une propriété
+eleve.taille = 180
+
+// En js, tous est objet
