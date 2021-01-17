7672d3b248b0c4dc8f844537396c363a043cae6a
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-01-17 14:51:48 +0100

    TP_onglet: ajout de css et js pour transition lors de la navigation entre les onglets

--- TP_onglet/app.js
+++ TP_onglet/app.js
@@ -1,7 +1,13 @@
 (function () {
-    var afficherOnglet = function (a) {
+    var afficherOnglet = function (a, animations) {
+        if (animations === undefined) {
+            animations = true;
+        }
         var li = a.parentNode;
         var div = a.parentNode.parentNode.parentNode;
+
+        var activeTab = div.querySelector('.tab-content.active'); //contenu actif
+        var aAfficher = div.querySelector(a.getAttribute('href')); //contenu a afficher
 
         if (li.classList.contains('active')) {
             return false;
@@ -13,10 +19,32 @@
         li.classList.add('active');
 
         // On retire la class active sur le contenu actif
-        div.querySelector('.tab-content.active').classList.remove('active');
+        //div.querySelector('.tab-content.active').classList.remove('active');
 
         // On ajoute la class active sur le contenu correspondant à notre clic
-        div.querySelector(a.getAttribute('href')).classList.add('active');
+        //div.querySelector(a.getAttribute('href')).classList.add('active');
+
+        if (animations) {
+            activeTab.classList.add('fade');
+            activeTab.classList.remove('in');
+            var transitionend = function () {
+                this.classList.remove('fade');
+                this.classList.remove('active');
+                aAfficher.classList.add('active');
+                aAfficher.classList.add('fade');
+                aAfficher.offsetWidth;
+                aAfficher.classList.add('in');
+                activeTab.removeEventListener('transitionend', transitionend)
+                activeTab.removeEventListener('webkitTransitionEnd', transitionend)
+                activeTab.removeEventListener('oTransitionEnd', transitionend)
+            }
+            activeTab.addEventListener('transitionend', transitionend)
+            activeTab.addEventListener('webkitTransitionEnd', transitionend)
+            activeTab.addEventListener('oTransitionEnd', transitionend)
+        } else {
+            aAfficher.classList.add('active');
+            activeTab.classList.remove('active');
+        }
     }
 
 
@@ -26,10 +54,15 @@
             afficherOnglet(this);
         })
 
-        var hash = window.location.hash;
-        var a = document.querySelector('a[href="' + hash + '"]');
-        if (a !== null && !a.parentNode.classList.contains('active')) {
-            afficherOnglet(a);
+        var hashChange = function (e) {
+            var hash = window.location.hash;
+            var a = document.querySelector('a[href="' + hash + '"]');
+            if (a !== null && !a.parentNode.classList.contains('active')) {
+                afficherOnglet(a, e !==undefined);
+            }
         }
+
+        window.addEventListener('hashChange', hashChange);
+        hashChange();
     }
 })()
