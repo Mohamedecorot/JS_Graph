8776cccf826696e110fba7fd771c538b13e5bb46
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-01-18 20:46:57 +0100

    TP_menucollant: ajout fonction scrollY et onScroll

---
+++ TP_menucollant/app.js
@@ -0,0 +1,24 @@
+(function(){
+
+    var scrollY = function () {
+        var supportPageOffset = window.pageXOffset !== undefined;
+        var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+
+        return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+    }
+
+
+
+    var onScroll = function () {
+        var hasScrollClass = element.classList.contains('fixed');
+        if (scrollY() > top && !hasScrollClass) {
+            element.classList.add('fixed');
+            element.style.width = rect.width + "px";
+            element.parentNode.insertBefore(fake, element);
+        } else if (scrollY() < top && hasScrollClass) {
+            element.classList.remove('fixed');
+            element.parentNode.removeChild(fake, element);
+        }
+    }
+    window.addEventListener('scroll', onScroll);
+})()
