af88df10f03c76f23f7505efd6839aa5fa7b6097
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-01-18 21:58:16 +0100

    TP_menucollant: ajout de contenu js

--- TP_menucollant/app.js
+++ TP_menucollant/app.js
@@ -7,42 +7,51 @@
         return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
     }
 
-    // Variables
-    var element = document.querySelector('.menu');
-    var rect = element.getBoundingClientRect();
-    var top = rect .top + scrollY();
-    var fake = document.createElement('div');
-    fake.style.width = rect.width + "px";
-    fake.style.height = rect.height + "px";
+    var elements = document.querySelectorAll('[data-sticky]');
+    for (var i = 0; i < elements.length; i++) {
+        (function(element) {
+                // Variables
+                //var element = document.querySelector('.menu');
+                var rect = element.getBoundingClientRect();
+                var offset = parseInt(element.getAttribute('data-offset') || 0, 10);
 
-    // Fonctions
+                var top = rect .top + scrollY();
+                var fake = document.createElement('div');
+                fake.style.width = rect.width + "px";
+                fake.style.height = rect.height + "px";
 
-    var onScroll = function () {
-        var hasScrollClass = element.classList.contains('fixed');
-        if (scrollY() > top && !hasScrollClass) {
-            element.classList.add('fixed');
-            element.style.width = rect.width + "px";
-            element.parentNode.insertBefore(fake, element);
-        } else if (scrollY() < top && hasScrollClass) {
-            element.classList.remove('fixed');
-            element.parentNode.removeChild(fake);
-        }
-    }
+                // Fonctions
 
-    var onResize = function () {
-        element.style.width = "auto";
-        element.classList.remove('fixed');
-        fake.style.display = "none";
-        rect = element.getBoundingClientRect();
-        top = rect .top + scrollY();
-        fake.style.width = rect.width + "px";
-        fake.style.height = rect.height + "px";
-        fake.style.display = "block";
-        onscroll();
-    }
+                var onScroll = function () {
+                    var hasScrollClass = element.classList.contains('fixed');
+                    if (scrollY() > top - offset && !hasScrollClass) {
+                        element.classList.add('fixed');
+                        element.style.top = offset + "px";
+                        element.style.width = rect.width + "px";
+                        element.parentNode.insertBefore(fake, element);
+                    } else if (scrollY() < top - offset && hasScrollClass) {
+                        element.classList.remove('fixed');
+                        element.parentNode.removeChild(fake);
+                    }
+                }
+
+                var onResize = function () {
+                    element.style.width = "auto";
+                    element.classList.remove('fixed');
+                    fake.style.display = "none";
+                    rect = element.getBoundingClientRect();
+                    top = rect .top + scrollY();
+                    fake.style.width = rect.width + "px";
+                    fake.style.height = rect.height + "px";
+                    fake.style.display = "block";
+                    onscroll();
+                }
 
 
-    // Listener
-    window.addEventListener('scroll', onScroll);
-    window.addEventListener('resize', onResize);
+                // Listener
+                window.addEventListener('scroll', onScroll);
+                window.addEventListener('resize', onResize);
+                    })(elements[i])
+                }
+
 })()
