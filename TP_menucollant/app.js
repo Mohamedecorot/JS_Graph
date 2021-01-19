b5bdf42f80b29b76e63f043af9c9d0b8396143d6
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-01-19 20:55:18 +0100

    TP_menucollant: fin

--- TP_menucollant/app.js
+++ TP_menucollant/app.js
@@ -7,50 +7,54 @@
         return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
     }
 
-    var elements = document.querySelectorAll('[data-sticky]');
-    for (var i = 0; i < elements.length; i++) {
-        (function(element) {
+    var makeSticky = function (element) {
                 // Variables
                 //var element = document.querySelector('.menu');
                 var rect = element.getBoundingClientRect();
                 var offset = parseInt(element.getAttribute('data-offset') || 0, 10);
                 if (element.getAttribute('data-constraint')){
-                    var constraint = document.document.querySelector(element.getAttribute('data-constraint'));
+                    var constraint = document.querySelector(element.getAttribute('data-constraint'));
                 } else {
                     var constraint = document.body;
                 }
                 var constraintRect = constraint.getBoundingClientRect();
                 var constraintBottom = constraintRect.top + scrollY() + constraintRect.height - offset - rect.height;
-                var top = rect .top + scrollY();
+                var top = rect.top + scrollY();
                 var fake = document.createElement('div');
                 fake.style.width = rect.width + "px";
                 fake.style.height = rect.height + "px";
 
                 // Fonctions
-
                 var onScroll = function () {
-                    if (scrollY() > constraintBottom) {
+                    if (scrollY() > constraintBottom && element.style.position != 'absolute') {
                         element.style.position = 'absolute';
                         element.style.bottom = '0';
                         element.style.top = 'auto';
-                    } else if (scrollY() > top - offset) {
+                    } else if (scrollY() > top - offset && scrollY() > constraintBottom && element.style.position != 'fixed') {
                         element.classList.add('fixed');
                         element.style.position = 'fixed';
                         element.style.top = offset + "px";
                         element.style.bottom = 'auto';
                         element.style.width = rect.width + "px";
                         element.parentNode.insertBefore(fake, element);
-                    } else if (scrollY() < top - offset) {
+                    } else if (scrollY() < top - offset && element.style.position != 'static') {
                         element.classList.remove('fixed');
-                        element.parentNode.removeChild(fake);
+                        element.style.position = 'static';
+                        if (element.parentNode.contains(fake)) {
+                            element.parentNode.removeChild(fake);
+                        }
                     }
                 }
 
                 var onResize = function () {
                     element.style.width = "auto";
                     element.classList.remove('fixed');
+                    element.style.position = "static";
                     fake.style.display = "none";
                     rect = element.getBoundingClientRect();
+                    constraintRect = constraint.getBoundingClientRect();
+                    constraintBottom = constraintRect.top + scrollY() + constraintRect.height - offset - rect.height;
+                    top = rect.top + scrollY();
                     top = rect .top + scrollY();
                     fake.style.width = rect.width + "px";
                     fake.style.height = rect.height + "px";
@@ -62,7 +66,11 @@
                 // Listener
                 window.addEventListener('scroll', onScroll);
                 window.addEventListener('resize', onResize);
-                    })(elements[i])
-                }
+    }
+
+    var elements = document.querySelectorAll('[data-sticky]');
+    for (var i = 0; i < elements.length; i++) {
+        makeSticky(elements[i]);
+    }
 
 })()
