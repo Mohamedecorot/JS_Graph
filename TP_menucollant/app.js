404e7a89d3c9789180c6e406efd6cdbcb01eb7ca
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-01-19 20:20:07 +0100

    TP_menucollant: ajout de contenu js

--- TP_menucollant/app.js
+++ TP_menucollant/app.js
@@ -14,7 +14,13 @@
                 //var element = document.querySelector('.menu');
                 var rect = element.getBoundingClientRect();
                 var offset = parseInt(element.getAttribute('data-offset') || 0, 10);
-
+                if (element.getAttribute('data-constraint')){
+                    var constraint = document.document.querySelector(element.getAttribute('data-constraint'));
+                } else {
+                    var constraint = document.body;
+                }
+                var constraintRect = constraint.getBoundingClientRect();
+                var constraintBottom = constraintRect.top + scrollY() + constraintRect.height - offset - rect.height;
                 var top = rect .top + scrollY();
                 var fake = document.createElement('div');
                 fake.style.width = rect.width + "px";
@@ -23,13 +29,18 @@
                 // Fonctions
 
                 var onScroll = function () {
-                    var hasScrollClass = element.classList.contains('fixed');
-                    if (scrollY() > top - offset && !hasScrollClass) {
+                    if (scrollY() > constraintBottom) {
+                        element.style.position = 'absolute';
+                        element.style.bottom = '0';
+                        element.style.top = 'auto';
+                    } else if (scrollY() > top - offset) {
                         element.classList.add('fixed');
+                        element.style.position = 'fixed';
                         element.style.top = offset + "px";
+                        element.style.bottom = 'auto';
                         element.style.width = rect.width + "px";
                         element.parentNode.insertBefore(fake, element);
-                    } else if (scrollY() < top - offset && hasScrollClass) {
+                    } else if (scrollY() < top - offset) {
                         element.classList.remove('fixed');
                         element.parentNode.removeChild(fake);
                     }
