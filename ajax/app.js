1a92f68d52426e5a8beefe6a43bf1015b0d3f1a4
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-01-19 21:22:48 +0100

    ajax: ajout de la fonction getHttpRequest

---
+++ ajax/app.js
@@ -0,0 +1,33 @@
+var getHttpRequest = function () {
+    // https://developer.mozilla.org/fr/docs/AJAX/Premiers_pas
+
+var httpRequest = false;
+
+if (window.XMLHttpRequest) { //Mozilla, Safari
+    httpRequest = new XMLHttpRequest();
+    if (httpRequest.overrideMimeType) {
+        httpRequest.overrideMimeType('text/xml');
+
+    }
+}
+else if (window.ActiveXObject) { // IE
+    try {
+        httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+    }
+    catch (e) {
+        try {
+            httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        catch (e) {}
+    }
+}
+
+if (!httpRequest) {
+    alert('Abandon :( Impossible de créer une instance XMLHTTP');
+    return false;
+}
+
+return httpRequest;
+}
+
+var httpRequest = getHttpRequest();
