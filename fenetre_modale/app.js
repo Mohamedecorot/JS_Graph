706ebe1431e1ef0132f99cfb262971e453cba81d
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-02-01 17:35:21 +0100

    fenetre modal: ajout de la partie ajax

--- fenetre_modale/app.js
+++ fenetre_modale/app.js
@@ -3,9 +3,14 @@ const focusableSelector = 'button, a, input, textarea'
 let focusables = []
 let previouslyFocusedElement = null
 
-const openModal = function (e) {
+const openModal = async function (e) {
     e.preventDefault()
-    modal = document.querySelector(e.target.getAttribute('href'))
+    const target = e.target.getAttribute('href')
+    if (target.startsWith('#')) {
+        modal = document.querySelector(target)
+    } else {
+        modal = await loadModal(target)
+    }
     focusables = Array.from(modal.querySelectorAll(focusableSelector))
     previouslyFocusedElement = document.querySelector(':focus')
     modal.style.display = null
@@ -54,6 +59,18 @@ const focusModal = function (e) {
 
 const stopPropagation = function (e) {
     e.stopPropagation()
+}
+
+const loadModal = async function (url) {
+    // TODO: Afficher un loader pour indiquer a l'utilisateur que ça charge
+    const target = '#' + url.split('#')[1]
+    const exitingModal = document.querySelector(target)
+    if (exitingModal !== null) return exitingModal
+    const html = await fetch(url).then(response => response.text())
+    const element = document.createRange().createContextualFragment(html).querySelector(target)
+    if (element === null) throw `L'élément ${target} n'a pas été trouvé dans la page ${url}`
+    document.body.append(element)
+    return element
 }
 
 document.querySelectorAll('.js-modal').forEach(a => {
